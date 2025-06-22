5a1afc595401d200df99793174b4cda3affe40cf
Kezia Sihombing <keziasihombing184@gmail.com>
2025-06-22 13:07:55 +0700

    update

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
 
-$controller = $_GET['c'] ?? 'Calendar';
-$method = $_GET['m'] ?? 'landingPage';
+$controller = $_GET['c'] ?? 'Login';
+$method = $_GET['m'] ?? 'loginAction';
 
 require_once "controller/Controller.class.php";
-require_once "controller/$controller.class.php";
 
-//run
+$controllerFile = "controller/$controller.class.php";
+
+if (!file_exists($controllerFile)) {
+    die("Controller '$controller' not found.");
+}
+
+require_once $controllerFile;
+
+if (!class_exists($controller)) {
+    die("Class '$controller' does not exist.");
+}
+
 $c = new $controller;
+
+if (!method_exists($c, $method)) {
+    die("Method '$method' does not exist in controller '$controller'.");
+}
+
 $c->$method();
